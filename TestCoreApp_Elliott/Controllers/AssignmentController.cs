fb7988951a38fc5041266c8dd82efc3343d22990
Joshua Elliott <jackal_ttoille@yahoo.com>
2023-07-02 23:12:12 -0500

    Adding Assignments6_1

---
+++ TestCoreApp_Elliott/Controllers/AssignmentController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestCoreApp_Elliott.Models;
+
+namespace TestCoreApp_Elliott.Controllers
+{
+    public class AssignmentController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Assignment6_1(int accessLevel)
+        {
+            List<Student> students = GetStudents(accessLevel);
+
+            AssignmentViewModel viewModel = new AssignmentViewModel
+            {
+                Students = students,
+                AccessLevel = accessLevel
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        private List<Student> GetStudents(int accessLevel)
+        {
+            List<Student> students = new List<Student>();
+
+            students.Add(new Student { FirstName = "John", LastName = "BonJovi", Grade = "B" });
+            students.Add(new Student { FirstName = "Lars", LastName = "Ulrich", Grade = "F" });
+            students.Add(new Student { FirstName = "Maynard", LastName = "James Keenan", Grade = "A" });
+
+            return students;
+        }
+    }
+}
---
+++ TestCoreApp_Elliott/Models/AssignmentViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TestCoreApp_Elliott.Models
+{
+    public class AssignmentViewModel
+    {
+        public List<Student> Students { get; set; }
+        public int AccessLevel { get; set; }
+    }
+}
