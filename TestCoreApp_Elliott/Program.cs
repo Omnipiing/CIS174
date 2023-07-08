f4b1e15662c72467735a9909d54aec377c0a955b
Omnipiing <jackal_ttoille@yahoo.com>
2023-07-08 16:30:26 -0500

    added asp-area to links for filtering

--- TestCoreApp_Elliott/Program.cs
+++ TestCoreApp_Elliott/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<OlympicsContext>(options =>
-	options.UseSqlServer(builder.Configuration.GetConnectionString("CountryContext")));
+	options.UseSqlServer(builder.Configuration.GetConnectionString("OlympicsContext")));
 
 var app = builder.Build();
 
